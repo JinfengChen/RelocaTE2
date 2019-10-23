a936b99b2f6268c9a71c54996078eba372ac8247
JinfengChen <jinfeng7chen@gmail.com>
2019-10-23 13:16:21 -0700

    update

--- git.sh
+++ git.sh
@@ -11,3 +11,10 @@ git commit -m "update"
 git remote add update https://github.com/JinfengChen/RelocaTE2_release.git
 git push
 
+echo "new rep for stajich lab relocate2"
+git init
+git pull https://github.com/stajichlab/RelocaTE2.git
+git rm test_data.tar.gz
+git commit -m "remove large test data"
+git remote add update https://github.com/stajichlab/RelocaTE2.git
+git push -u update master
